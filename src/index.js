98067a5526510d9b0316a14643c1e67b991b7fb1
Ángel Uriel González Urrutia <115201033+UriProtocol@users.noreply.github.com>
2023-01-13 14:43:25 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<title>GRUPO 5B</title>
+<h1>Mi primera práctica con Sandbox</h1>
+<h2>Ángel Uriel González Urrutia</h2>
+<br>
+<div>
+  <p><span class="verde">U</span>niversidad <span class="verde">T</span>ecnológica de <span class="amarillo">D</span>urango</p><br>
+  <button><a href="https://github.com/UriProtocol/React" target="_blank" rel="noopener noreferrer">Mi repositorio de GitHub</a></button>
+</div>
+`;
